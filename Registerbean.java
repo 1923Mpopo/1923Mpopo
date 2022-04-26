c196043b7a6f530c3f604cb1c8ca5133eb13794c
Mamonamoli Mpopo <91981851+1923Mpopo@users.noreply.github.com>
2022-04-26 11:19:48 +0200

    initial commit

---
+++ Registerbean.java
@@ -0,0 +1,59 @@
+package Mpopo;
+
+import java.io.Serializable;
+
+public class Registerbean implements Serializable{
+	
+	 private int memberID;
+	 private String uname;
+	 private String fname;
+	 private String pass;
+	 private String email;
+	 private int phone;
+	 private String usertype;
+	 
+	public int getMemberID() {
+		return memberID;
+	}
+	public void setMemberID(int memberID) {
+		this.memberID = memberID;
+	}
+	public String getUname() {
+		return uname;
+	}
+	public void setUname(String uname) {
+		this.uname = uname;
+	}
+	public String getFname() {
+		return fname;
+	}
+	public void setFname(String fname) {
+		this.fname = fname;
+	}
+	public String getPass() {
+		return pass;
+	}
+	public void setPass(String pass) {
+		this.pass = pass;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public int getPhone() {
+		return phone;
+	}
+	public void setPhone(int phone) {
+		this.phone = phone;
+	}
+	public String getUsertype() {
+		return usertype;
+	}
+	public void setUsertype(String usertype) {
+		this.usertype = usertype;
+	}
+	 
+
+}
